f46f18d24ee174bafcc399194bf90145ef8f8bba
SebastianSzczypkowski <szczsebastian146@gmail.com>
2021-07-20 21:44:05 +0200

    Spotify api

---
+++ src/main/java/pl/szczypkowski/spotitunes/SpotiTunesApplication.java
@@ -0,0 +1,13 @@
+package pl.szczypkowski.spotitunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotiTunesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotiTunesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/szczypkowski/spotitunes/UserInfo.java
@@ -0,0 +1,15 @@
+package pl.szczypkowski.spotitunes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserInfo {
+
+    @GetMapping("/user")
+    public Principal get(Principal principal){
+        return principal;
+    }
+}
